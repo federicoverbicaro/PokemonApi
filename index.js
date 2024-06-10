d4ae67711719c93e552f260a637cb75e063e6b24
federicoverbicaro <federico.verbicaro@icloud.com>
2024-06-10 16:25:17 +0200

    feature Pokedex esterno

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
+// chiamata api pokemon
 async function fetchData() {
-
 
     try {
         const pokemonName = document.getElementById('pokemonName').value.toLowerCase()
-        
+
 
         const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
 
@@ -20,11 +19,11 @@ async function fetchData() {
         pokemonName2.textContent = pokemonName
 
         //id pokemon
-        const NumeroPokemon =  document.getElementById('NumeroPokemon')
+        const NumeroPokemon = document.getElementById('NumeroPokemon')
         const pokemonId = data.id
         NumeroPokemon.textContent = `#${pokemonId}`
 
-        if(!pokemonId){
+        if (!pokemonId) {
             NumeroPokemon.textContent = ''
         }
 
@@ -53,10 +52,10 @@ async function fetchData() {
         const pokemonType2 = data.types[1] ? data.types[1].type.name : null
 
 
-        if(pokemonType){
-            pokemonDati2.textContent = pokemonType
+        if (pokemonType) {
+            pokemonDati2.textContent = `${pokemonType} /`
             pokemonDati3.textContent = pokemonType2 ? pokemonType2 : ''
-        }else{
+        } else {
             pokemonDati2.textContent = ''
             pokemonDati2.textContent = ''
             tipoNonEsiste.textContent = 'non ci sono tipi'
@@ -72,22 +71,64 @@ async function fetchData() {
         const pokemonAbilites = data.abilities[0].ability.name
         const pomekonAbilites2 = data.abilities[1].ability.name
 
-        abilityPokemon.textContent = pokemonAbilites
+        abilityPokemon.textContent = `${pokemonAbilites} /`
         ability2Pokemon.textContent = pomekonAbilites2
-        
-     
-        
 
-        console.log('datiarray',data)
-        
+
+        //vita pokemon
+        const hp = document.getElementById('hp')
+        const vita = data.stats[0].base_stat
+        hp.textContent = vita
+
+
+        // attaco pokemo
+        const att = document.getElementById('att')
+        const attac = data.stats[1].base_stat
+        att.textContent = attac
+
+
+        // difesa pokemon
+        const dif = document.getElementById('dif')
+        const diffesa = data.stats[2].base_stat
+        dif.textContent = diffesa
+
+
+        //attaco speciale pokemon
+        const atk = document.getElementById('atk')
+        const attSpeciale = data.stats[3].base_stat
+        atk.textContent = attSpeciale
+
+
+        // difesa speciale pokemon
+        const def = document.getElementById('def')
+        const defSpeciale = data.stats[4].base_stat
+        def.textContent = defSpeciale
+
+
+        // velocità pokemon
+        const vel = document.getElementById('vel')
+        const velocita = data.stats[5].base_stat
+        vel.textContent = velocita
+
+
+        console.log('datiarray', data)
+        console.log(vita)
     }
     catch (error) {
         console.error(error);
     }
 }
-
 fetchData()
 
+// funzione button per pokedex esterno
+function togglePokedex() {
+    const pokedex = document.getElementById('pokedex');
+    if (pokedex.style.display === 'none' || pokedex.style.display === '') {
+        pokedex.style.display = 'block';
+    } else {
+        pokedex.style.display = 'none';
+    }
 
-
+    
+}
 
